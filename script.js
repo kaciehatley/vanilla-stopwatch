e8d3b93ed421a6e78a0bacd02002a173041bc370
Kacie Hatley <kaciehatley@gmail.com>
2020-06-18 22:29:12 -0400

    timer functioning. building function to simplify adding placeholder zero for single digit numbers

--- script.js
+++ script.js
@@ -18,14 +18,51 @@ window.onload = function() {
 
 // Timer Begins When "Start" Is Clicked
 start.addEventListener("click", function() {
+    // Code is repeated every 10 milliseconds
     let timer = window.setInterval(function(){
-        milli += 01;
-        milliText.innerHTML = milli;
-        if (seconds==10 && milli==99) {
-            seconds = 00;
+        // function updateWatch(variable, element) {
+        //     variable +=1;
+        //     if(variable.toString().length<2) {
+        //         element.innerHTML="0"+variable;
+        //         console.log("if statement");
+        //     }
+        //     else {
+        //         element.innerHTML=variable;
+        //         console.log("else statement");
+        //     }
+        // }
+        // updateWatch(milli, milliText);
+
+        // if (milli==99) {
+        //     milli = 00;
+        //     updateWatch(seconds,secondsText);
+        // }
+
+        // if (seconds==10 && milli==99) {
+        //     seconds = 00;
+        //     updateWatch(minutes, minutesText);
+        // }
+
+        // Add one hundredth of a second
+        milli += 1;
+
+        // // If milliseconds are only one digit long, add 0 to beginning
+        if (milli.toString().length < 2) {
+            milliText.innerHTML = "0"+milli;
+        }
+        // // If milliseconds are two digits long, update DOM and continue
+        if (milli.toString().length == 2) {
+            milliText.innerHTML = milli;
+        }
+
+        // Add minute after 00:59:99
+        if (seconds==9 && milli==99) {
+            seconds=00;
             minutes+=1;
             minutesText.innerHTML = minutes;
         }
+
+        // If there are 99/100 of a second, add unit to seconds
         if (milli==99) {
             milli = 00;
             seconds+=1;
